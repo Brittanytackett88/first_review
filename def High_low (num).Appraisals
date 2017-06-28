40b9604bb647f2ca36fc4486cb42a43a2365f8dd
brittany <brittanyntackett88@gmail.com>
2017-06-28 12:55:42 -0400

    trials and errors

---
+++ def High_low (num).Appraisals
@@ -0,0 +1,13 @@
+def High_low (num)
+num = rand(num)
+if > num
+puts "too high"
+else
+if < num
+puts too low
+end
+end
+end
+def high_low(9)
+
+
---
+++ highlow.rb
@@ -0,0 +1,18 @@
+def High_low (num)
+var = rand(num)
+if > var
+puts "too high"
+
+if < var
+puts "too low"
+elsif
+	if var==6
+		puts "You win"
+	else puts var
+	end 
+end
+end
+
+def high_low(9)
+
+
